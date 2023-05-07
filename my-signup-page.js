2393c45764f3bf8b04f60efe02a74357301f8beb
thatkhay <johnkingsleyegeonu@gmail.com>
2023-05-07 08:39:35 -0700

    added an event listener to the mark all as read

--- my-signup-page.js
+++ my-signup-page.js
@@ -2,25 +2,41 @@ const whitePage = document.querySelector('.mainWhiteDiv');
 const unReadNotification = document.querySelector('.spanNo');
 const notificationFour = document.querySelector('.notDiv4');
 const message = document.querySelector('.message');
+const markAllAsRead = document.querySelector('.topRight');
 
 let notificationCount = 7;
 
 unReadNotification.innerHTML = notificationCount;
 const notificationOne = document.querySelector('.notDiv1');
-
+const icons = document.querySelectorAll('.icon');
 
 
 const notifications = document.querySelectorAll('.notification');
 notifications.forEach(notification => {
     notification.addEventListener('click', () => {
-        const icons = document.querySelectorAll('.icon');
-        icons.forEach(icon => {
-            icon.style.display = 'none'
-        });
       notification.style.backgroundColor = 'white';
-      
+      for (let i = 0; i < notifications.length; i++) {
+        icons[i].style.display = 'none'
+    
+      }
     });
   });
+
+
+  markAllAsRead.addEventListener('click', markAll)
+
+  function markAll() {
+    for (let i = 0; i < notifications.length; i++) {
+      notifications[i].style.backgroundColor = 'white'
+  
+    }
+    for (let i = 0; i < notifications.length; i++) {
+      icons[i].style.display = 'none'
+  
+    }
+
+    
+  }
 
 notificationFour.addEventListener('click', onTap)
 
